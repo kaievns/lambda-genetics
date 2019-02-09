fc47fcc3e35916e087155c46102ae65d12fb09ce
Nikolay Nemshilov <nemshilov@gmail.com>
2019-02-09 11:23:04 +1100

    Making a thing that calls another thing

--- handler.ts
+++ handler.ts
@@ -1,11 +1,57 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { Lambda } from 'aws-sdk';
 
-export const hello: APIGatewayProxyHandler = async (event, context) => {
+const lambda = new Lambda();
+const letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'.split('');
+
+export const generation: APIGatewayProxyHandler = async () => {
+  const sequences = new Array(100).fill(null).map(() => {
+    const randoms = letters.sort(() => Math.random() > 0.5 ? 1 : -1);
+    return randoms.join('');
+  });
+
+  const result = await Promise.all(sequences.map(sequence => 
+    new Promise(resolve => {
+      const params = {
+        FunctionName: 'lambda-genetics-dev-calculate',
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify(sequence)
+      };
+
+      lambda.invoke(params, (err, data) => {
+        let result = null;
+
+        if (err) {
+          console.error('failed to process', sequence, err);
+        } else {
+          try {
+            const parsedData = JSON.parse(data.Payload as string);
+            result = JSON.parse(parsedData.body);
+          } catch (error) {
+            console.error('failed to parse response from calculation of', sequence, err);
+          }
+        }
+
+        resolve(result);
+      });
+    })
+  ));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result, null, 2),
+  };
+}
+
+export const calculate: APIGatewayProxyHandler = async (sequence) => {
+  const startedAt = Date.now();
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+      sequence,
+      score: Math.random(),
+      time: Date.now() - startedAt
+    })
+  }
 }
